9eae0b50561ee2f2f34fec3a770c28a7d9ab35d1
icymind <icymindt@gmail.com>
2017-09-02 13:13:37 +0800

    remove useless lib

--- src/renderer/lib/helper.js
+++
@@ -1,9 +0,0 @@
-const myPath = require('path')
-
-function getAppDir () {
-  return process.env.APPDATA || (process.platform === 'darwin' ? myPath.join(process.env.HOME, 'Library', 'Application Support') : '/var/local')
-}
-
-module.exports = {
-  getAppDir
-}
